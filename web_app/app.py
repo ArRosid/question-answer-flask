fdd9a0a250b332dbae3df8b331efd749fe5b9689
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-25 15:31:53 +0700

    Create a Register function work

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import g, Flask, render_template, request, redirect, url_for
+import dbcon
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -10,6 +12,24 @@ def close_db(error):
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    db = dbcon.get_db()
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        hashed_password = generate_password_hash(password, method='sha256')
+        db.execute(''' insert into users (name, password, expert, admin)
+                       values (?, ?, ?, ?)''',
+                       [username, hashed_password, '0', '0'])
+        db.commit()
+
+        return redirect(url_for('index'))
+            
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001", debug=True)
