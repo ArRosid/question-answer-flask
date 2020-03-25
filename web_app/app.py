e75dcf60fbf40836273db5a04bb1b0059567835a
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-25 19:59:14 +0700

    Add user to session when logged in & Add logout function

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, g, redirect, render_template, request, url_for
+import os
+
+from flask import (Flask, g, redirect, render_template, request, session,
+                   url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 import dbcon
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
 
 @app.teardown_appcontext
 def close_db(error):
@@ -12,7 +16,12 @@ def close_db(error):
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    user = None
+    
+    if 'user' in session:
+        user = session["user"]
+
+    return render_template("home.html", user=user)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -40,15 +49,22 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        user_cur = db.execute("select id, name, password from users where name = ? ", [username])
+        user_cur = db.execute("select id, name, password from users where name = ? ",
+                               [username])
         user = user_cur.fetchone()
 
         if check_password_hash(user["password"], password):
-            return "<h1>Login Successful!</h1>"
+            session["user"] = user["name"]
+            return redirect(url_for("index"))
         else:
-            return "<h1>Login failed</h1>"
+            return redirect(url_for("login"))
 
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001", debug=True)
