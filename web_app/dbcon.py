ee5432dce7a2501c6141c92cd00e4c9c94387b92
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-31 07:03:14 +0700

    Add postgres to the project

--- web_app/dbcon.py
+++ web_app/dbcon.py
@@ -1,31 +1,57 @@
 from flask import g
 import sqlite3
+import os
+import psycopg2
+from psycopg2.extras import DictCursor
+
+# def connect_db():
+#     sql = sqlite3.connect("./questions.db")
+#     sql.row_factory = sqlite3.Row
+#     return sql
+
+# def get_db():
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = connect_db()
+#     return g.sqlite_db
+
+# def extract_dbs(db_result):
+#     result = []
+
+#     if len(db_result) == 0:
+#         return result
+        
+#     for d in db_result:
+#         d = dict(d)
+#         result.append(d)
+
+#     return result
+
+# def extract_db(db_result):
+
+#     if len(db_result) == 0:
+#         return {}
+
+#     return dict(db_result)
+
 
 def connect_db():
-    sql = sqlite3.connect("./questions.db")
-    sql.row_factory = sqlite3.Row
-    return sql
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect()
 
-def extract_dbs(db_result):
-    result = []
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = connect_db()[0]
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = connect_db()[1]
 
-    if len(db_result) == 0:
-        return result
-        
-    for d in db_result:
-        d = dict(d)
-        result.append(d)
+    return g.postgres_db_cur
 
-    return result
-
-def extract_db(db_result):
-
-    if len(db_result) == 0:
-        return {}
-
-    return dict(db_result)
+def init_db():
+    db = connect_db()
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+    db[0].close()
